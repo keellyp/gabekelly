91c23da5025ea9c1de5b13c6f71fd28142588da6
Kelly Phan <keelly.p@gmail.com>
2018-10-11 17:22:34 +0200

    🚧 Try to create page for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+/** 
+ * Implement the Gatsby API "createPages"
+ * This is called once the data layer is bootstrapped to let plugins create pages from datas
+*/
+// const path = require('path')
 
- // You can delete this file if you're not using it
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   return new Promise((resolve, reject) => {
+//     const tripTemplate = path.resolve(`src/templates/trip.js`)
+
+    // Query for markdown nodes to use in creating pages.
+    // resolve(
+    //   graphql(
+    //     `
+    //       {
+    //         allMarkdownRemark(limit: 1000) {
+    //           edges {
+    //             node {
+    //               frontmatter {
+    //                 path
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `
+    //   )
+    //   .then(result => {
+    //     // Handling errors  
+    //     if (result.errors) {
+    //       reject(result.errors)
+    //     }
+
+    //     // Create pages for each markdown file.
+    //     const nodes = result.data.allMarkdownRemark.edges
+    //     nodes.forEach(({ node }) => {
+    //       const path = node.frontmatter.path
+    //       createPage({ path, component: tripTemplate, context: { path } })
+    //     })
+    //   })
+    // )
+  // })
+// }
