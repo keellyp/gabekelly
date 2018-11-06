e9501a615b55735a2b0658c0ba9eefe07deb4a45
Kelly Phan <kelly.phan@hetic.net>
2018-11-07 00:00:13 +0100

    📝 Add site info

---
+++ site-config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+  siteTitle: 'Gabe & Kelly',
+  siteDescription: 'Gabe&Kelly keeps our memories alive.',
+  siteUrl: 'https://gabe.kellyphan.fr',
+  siteLogo: path.resolve(__dirname, 'static/img/logo.jpg'),
+}
