81de6a211ec0b532e33cd36836a4ebf85e1649b3
Kelly Phan <kelly.phan@hetic.net>
2018-11-06 23:59:52 +0100

    🐛 Fix node file with json transformer

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,29 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
 /**
  * Implement the Gatsby API "onCreateNode"
  * This is called when a new node is created/updated : use to add slug to node field
-*/
+ */
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `trips` })
-    createNodeField({ node, name: `slug`, value: slug })
+  if (node.internal.type === 'Json') {
+    const slug = createFilePath({ node, getNode, basePath: 'trips' })
+    createNodeField({ node, name: 'slug', value: slug })
   }
 }
 
 /**
  * Implement the Gatsby API "createPages"
  * This is called once the data layer is bootstrapped to let plugins create pages from datas
-*/
+ */
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allMarkdownRemark {
+        allJson {
           edges {
             node {
               fields {
@@ -33,17 +33,16 @@ exports.createPages = ({ graphql, actions }) => {
           }
         }
       }
-    `
-    ).then(result => {
+    `).then(result => {
       if (result.errors) {
         reject(result.errors)
       }
 
-      const nodes = result.data.allMarkdownRemark.edges
+      const nodes = result.data.allJson.edges
       nodes.forEach(({ node }) => {
         createPage({
           path: node.fields.slug,
-          component: path.resolve(`./src/templates/trip.js`),
+          component: path.resolve('./src/templates/trip.js'),
           context: {
             // Data passed to context is available
             // in page queries as GraphQL variables.
@@ -51,7 +50,7 @@ exports.createPages = ({ graphql, actions }) => {
           },
         })
       })
-      
+
       resolve()
     })
   })
