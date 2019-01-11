ddd3f47c95e24a9ea303e253e9fc8994d0b56fbf
Kelly Phan <kelly.phan@hetic.net>
2019-01-11 23:53:45 +0100

    🎨 Improve structure

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Import all components
+
+export { default as AppCover } from './components/AppCover'
+export { default as AppTitle } from './components/AppTitle'
+export { default as Head } from './components/Head'
+export { default as Header } from './components/Header'
+export { default as Layout } from './components/Layout'
+export { default as TripContent } from './components/TripContent'
+export { default as TripFooter } from './components/TripFooter'
+export { default as TripHeader } from './components/TripHeader'
