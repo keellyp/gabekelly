f63a69b52170d5979e8f08b3afb5deeb8e463d4f
Kelly Phan <kelly.phan@hetic.net>
2019-01-20 14:34:55 +0100

    🐛 Fix breakpoints bug

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,16 +1,16 @@
-const size = {
-  mobile: '375px',
-  tablet: '560px',
-  tabletLandscape: '768px',
-  smallDesktop: '1024px',
-  largeDesktop: '1980px',
+export const size = {
+  mobile: 375,
+  tablet: 560,
+  tabletLandscape: 768,
+  smallDesktop: 1024,
+  largeDesktop: 1980,
 }
 
 export const device = {
-  mobile: `(max-width: ${size.mobile})`,
-  tablet: `(max-width: ${size.tablet})`,
-  tabletLandscape: `(max-width: ${size.tabletLandscape})`,
-  smallDesktop: `(max-width: ${size.smallDesktop})`,
-  mediumDesktop: `(max-width: ${size.largeDesktop})`,
-  largeDesktop: `(min-width: ${size.largeDesktop})`,
+  mobile: `(max-width: ${size.mobile}px)`,
+  tablet: `(max-width: ${size.tablet}px)`,
+  tabletLandscape: `(max-width: ${size.tabletLandscape}px)`,
+  smallDesktop: `(max-width: ${size.smallDesktop}px)`,
+  mediumDesktop: `(max-width: ${size.largeDesktop}px)`,
+  largeDesktop: `(min-width: ${size.largeDesktop}px)`,
 }
